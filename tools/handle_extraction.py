5119437cdd41b32da965fa670252518f7f48eea2
xinyangzhang <eyebrow0524@gmail.com>
2024-09-28 21:25:58 +0100

    all first three test cases run succesfully

--- tools/handle_extraction.py
+++ tools/handle_extraction.py
@@ -39,18 +39,36 @@ def data_extraction(api_response,response_extraction_in_excel):
     except json.JSONDecodeError as e:
         logger.error(f"Failed to parse response_extraction as JSON: {e}")
         return
+    # # version1
+    # if isinstance(response_extraction_in_excel, dict):
+    #   # Iterate over the key-value pairs in the dictionary
+    #     for key,value in response_extraction_in_excel.items():
+    #         try:
+    #             # extract teh value from api_response using jsonpath
+    #             value_to_save=jsonpath(api_response.json(), value)[0]
+    #             logger.info(f'data found from API response is {value_to_save}')
+    #             # set the attribute in EnvironmentalData class
+    #             setattr(EnviromentalData,key,value_to_save)
+    #             logger.info(f'data has been saved into the environmental files attribute is {EnviromentalData.__dict__}')
+    #         except Exception as e:
+    #             logger.error(f"Error extracting data for {key}: {e}")
+    # else:
+    #     logger.error("Invalid response_extraction format; must be a dictionary")
 
+    # version 2
     if isinstance(response_extraction_in_excel, dict):
+
       # Iterate over the key-value pairs in the dictionary
         for key,value in response_extraction_in_excel.items():
             value_to_save=None
-            if value.startswith('$'):
+            try:
+                if value.startswith("$"):
                 # extract teh value from api_response using jsonpath
-                value_to_save=jsonpath(api_response, value)[0]
-                logger.info(f'data found from API response is {value_to_save}')
-            elif v=='text':
-                value_to_save = api_response.text #In Python, when dealing with HTTP responses, you can use the .text attribute to get the response body as a string.
-                # set the attribute in EnvironmentalData class
+                    value_to_save=jsonpath(api_response.json(), value)[0]
+                    logger.info(f'data found from API response is {value_to_save}')
+                elif value=='text':
+                    value_to_save = api_response.text #In Python, when dealing with HTTP responses, you can use the .text attribute to get the response body as a string.
+                    # set the attribute in EnvironmentalData class
                 setattr(EnviromentalData,key,value_to_save)
                 logger.info(f'data has been saved into the environmental files attribute is {EnviromentalData.__dict__}')
             except Exception as e:
